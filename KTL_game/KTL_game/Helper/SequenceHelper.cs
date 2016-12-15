140efb472cbe812b82051c2b165a503da93f667d
SidLeniwiec <piotrmichalik17@wp.pl>
2016-12-15 14:40:27 +0100

    kek, chcialem ci sklepac klase, ale jednak teraz nie mam czasu ;/

---
+++ KTL_game/KTL_game/Helper/SequenceHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KTL_game.Helper
+{
+    public class SequenceHelper
+    {
+        public static int NTerm(int firstTerm, int step, int n)
+        {
+            int nTerm = 0;
+            nTerm = firstTerm + (n - 1) * step;
+
+            return nTerm;
+        }
+    }
+}
--- KTL_game/KTL_game/MainWindow.xaml.cs
+++ KTL_game/KTL_game/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KTL_game.Helper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
